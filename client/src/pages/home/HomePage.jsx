d009d672248051b8c8d71b123deb7a7ffb968ab1
KwinyarutP <kwinyarut2546@gmail.com>
2024-08-08 00:18:17 +0200

    add: component HomePage

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import Navbaruser from '../../components/Navbaruser';
+import Searchbar from '../../components/Searchbar'
 
 export const HomePage = () => {
   const [message, setMessage] = useState("");
 
   return (
     <div>
-   
-     
+      <Navbaruser/>
+      <Searchbar/>
     </div>
   )
 };
