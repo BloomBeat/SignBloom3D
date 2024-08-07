98d7b186f10b27cf385d86494934b8f3894e0d2a
KwinyarutP <kwinyarut2546@gmail.com>
2024-08-07 17:23:12 +0200

    add: VocabularyPage changesFileName

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,57 +1,23 @@
-import React from "react";
 import { useState, useEffect } from "react";
-import SearchBar from '../../components/Searchbar';
-import CustomBtn from '../../components/Botton';
-import api from '../../hooks/api';
+import Navbaruser from '../../components/Navbaruser';
 
-export const HomePage = () => {
+// client/src/components/Navbaruser.jsx
+export const Home = () => {
   const [message, setMessage] = useState("");
-  const [searchResults, setSearchResults] = useState([]); // hold search results
-  const [selectedSearch, setSelectedSearch] = useState(null); // hold the selected item
-  const [query, setQuery] = useState(''); // hold the search query
 
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      try {
-        const response = await api.get("/search"); // Replace with your API endpoint
-        if (response.data.vocabularySuggestions.length() !== 0){
-          setSearchResults(response.vocabularySuggestions);
-        }
-      } catch (error) {
-        console.error('Error fetching search results:', error);
-      }
-    };
-    fetchSearchResults();
-  }, []);
-
-  // Filter the search results based on the query
-  const filteredSearchResults =
-  query === ''
-    ? searchResults
-    : searchResults.filter((item) =>
-        item.name.toLowerCase().includes(query.toLowerCase())
-    );
-
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then((response) => response.json())
+  //     .then((data) => setMessage(data.message));
+  // }, []);
+  // return <div className="mt-10 flex w-full flex-col">{message}</div>;
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* <Navbaruser/> */}
-
-      <div className="flex-grow flex items-center justify-center">
-        <div className="flex flex-col items-center space-y-10">
-          <img src="/SignBloom3DLogo.png" alt="Public Image" className="w-max h-max object-cover" />
-
-          {/* Flex container for SearchBar and Button */}
-          <div className="flex items-center space-x-4">
-            <SearchBar
-            selectedSearch={selectedSearch}
-            setSelectedSearch={setSelectedSearch}
-            setQuery={setQuery}
-            filteredSearchResults={filteredSearchResults}
-            />
-            <CustomBtn label="ค้นหา"/>
-          </div>
-        </div>
+    <div>
+      <Navbaruser></Navbaruser>
+      <div>
+        <FilterStatus></FilterStatus>
       </div>
     </div>
+ 
   )
 };
