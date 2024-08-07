697bb40bba1bf4531c94a80cb712e304268e0a1f
KwinyarutP <kwinyarut2546@gmail.com>
2024-08-08 01:29:34 +0200

    edit: center Searchbar

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Navbaruser from '../../components/Navbaruser';
-import Searchbar from '../../components/Searchbar'
+import SearchBar from '../../components/Searchbar';
 
 // client/src/components/Navbaruser.jsx
 export const Home = () => {
@@ -13,9 +13,11 @@ export const Home = () => {
   // }, []);
   // return <div className="mt-10 flex w-full flex-col">{message}</div>;
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbaruser/>
-      <Searchbar/>
+      <div className="flex-grow flex items-center justify-center">
+        <SearchBar />
+      </div>
     </div>
   )
 };
