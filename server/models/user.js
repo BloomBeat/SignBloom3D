3d2e36e9eafa80df2be47b28b7938c54ae31c14f
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-07 12:11:24 +0700

    Update : Add UserID

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  _id: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstname: { type: String, required: true },
