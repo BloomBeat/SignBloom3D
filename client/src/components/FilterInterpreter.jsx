7770aa69cbfcd68e53b2747af48957b8a1628a44
NANAYEMYINTKYI <ayemyint.kyi2004@gmail.com>
2024-08-08 11:03:17 +0700

    add: Navbaruser , adjustfile

--- client/src/components/FilterInterpreter.jsx
+++ client/src/components/FilterInterpreter.jsx
@@ -2,33 +2,33 @@ import React from 'react'
 import {useState} from React
 import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/react'
 
-const interpr = [
+const interpreter = [
   {id: 1 ,name: 'ล่ามโทรทัศน์'},
   {id: 2 ,name: 'ล่ามทั่วไป'},
 ]
 
 function FilterInterpr(){
-  const [SelectedInterpr, setSelectedInterpr] = useState(interpr[0])
+  const [SelectInterpreter, setSelectInterpreter] = useState(interpreter[0])
   const [query, setQuery] = useState('')  
   const Filterinterpr =
     query == ''
-      ? interpr
-      : interpr.filter((interpr) =>{
-          return interpr.name.toLowerCase().includes(query.toLowerCase())
+      ? interpreter
+      : interpreter.filter((interpreter) =>{
+          return interpreter.name.toLowerCase().includes(query.toLowerCase())
       })
   
   return (
-    <Combobox value={SelectedInterpr} onChange={setSelectedInterpr} onClose={() => setQuery('')}>
+    <Combobox value={SelectInterpreter} onChange={setSelectInterpreter} onClose={() => setQuery('')}>
       <ComboboxInput
        aria-label='Assignee'
-       displayValue={(interpr)=> interpr?.name}
+       displayValue={(interpreter)=> interpreter?.name}
        onChange={(event) => setQuery(event.target.value)}
        />
     <ComboboxOptions anchor="bottom" className="border empty:invisible" >
-      {filteredinterpr.map((interpr)=>(
-        <ComboboxOption key={interpr.id} value={interpr} className="group flex gap-2 bg-white data-[focus]:bg-blue-100"> 
+      {filteredinterpr.map((interpreter)=>(
+        <ComboboxOption key={interpreter.id} value={interpreter} className="group flex gap-2 bg-white data-[focus]:bg-blue-100"> 
           <CheckIcon className="invisible size-5 group-data-[selected]:visible"/>
-          {interpr.name}
+          {interpreter.name}
         </ComboboxOption>
       ))}
     </ComboboxOptions>
@@ -39,4 +39,4 @@ function FilterInterpr(){
 
 export default FilterInterpr;
 
-//not fetch witd backend yet
+//not fetch with backend yet
---
+++ client/src/pages/home/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Navbaruser from "../../components/Navbaruser";
+
+export const Home = () => {
+  const [message, setMessage] = useState("");
+
+  //  useEffect(() => {
+  //    fetch("/api")
+  //      .then((response) => response.json())
+  //      .then((data) => setMessage(data.message));
+  //  }, []);
+  //  return <div className="mt-10 flex w-full flex-col">{message}</div>;
+  return (
+   <Navbaruser></Navbaruser>
+  )
+};
