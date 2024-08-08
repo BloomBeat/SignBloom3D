e07114592e394c995cc12b245300fc71efd79e73
KwinyarutP <kwinyarut2546@gmail.com>
2024-08-08 02:22:31 +0200

    edit: button component

--- client/src/components/BtnFind.jsx
+++ client/src/components/BtnFind.jsx
@@ -1,9 +1,9 @@
-function BtnDownload() {
+function BtnFind() {
   return (
     <button className="rounded bg-primary-base py-2 px-4 text-sm text-white hover:bg-sky-500 active:bg-sky-700">
-      ดาวน์โหลด
+      ค้นหา
     </button>
   );
 }
 
-export default BtnDownload;
+export default BtnFind;
