933c5d8009b6dfa82223a41680b9bfee4da76b05
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 22:46:05 +0700

    Add : validateRegister

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { connectToDatabase } from "./configs/db.config.js";
 import router from "./routes/vocab.js"; // Updated import
 import userRouter from "./routes/user.js"; // Updated import
+
 const app = express();
 
 // Connect to the databasew
@@ -11,7 +12,7 @@ connectToDatabase();
 // Middleware, routes, etc.
 app.use(express.json());
 app.use("/api", router);
-app.use("/api/login", userRouter); // api/users/
+app.use("/api/user", userRouter);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
