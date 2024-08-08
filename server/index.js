f731c5b39ec8fd4856190f05dc0e57f5cd97c258
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 21:25:12 +0700

    Edit : prettier and add userRouter

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 // /server/index.js
 import express from "express";
 import { connectToDatabase } from "./configs/db.config.js";
-import vocabRouter from "./routes/vocab.js"; // Updated import
-import ticketRouter from "./routes/ticket.js"; // Updated import
+import router from "./routes/vocab.js"; // Updated import
+import userRouter from "./routes/user.js"; // Updated import
 const app = express();
 
 // Connect to the database
@@ -10,9 +10,8 @@ connectToDatabase();
 
 // Middleware, routes, etc.
 app.use(express.json());
-app.use("/api/vocab", vocabRouter);
-app.use("/api/ticket", ticketRouter);
-
+app.use("/api", router);
+app.use("/api/users", userRouter); // api/users/
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
