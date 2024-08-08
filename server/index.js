5723447760afe9e52388e20e8ed7b5e0e06c6315
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 22:46:05 +0700

    Add : validateRegister

--- server/index.js
+++ server/index.js
@@ -3,15 +3,16 @@ import express from "express";
 import { connectToDatabase } from "./configs/db.config.js";
 import router from "./routes/vocab.js"; // Updated import
 import userRouter from "./routes/user.js"; // Updated import
+
 const app = express();
 
-// Connect to the database
+// Connect to the databasew
 connectToDatabase();
 
 // Middleware, routes, etc.
 app.use(express.json());
 app.use("/api", router);
-app.use("/api/login", userRouter); // api/users/
+app.use("/api/user", userRouter);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ server/middlewares/validateRegister.js
@@ -0,0 +1,48 @@
+const validateRegister = (req, res, next) => {
+  const {
+    email,
+    password,
+    firstname,
+    lastname,
+    age,
+    hearing_level,
+    interpreter_group,
+    curriculum,
+    curr_time,
+    institution,
+    picture_profile,
+    role,
+  } = req.body;
+
+  if (
+    !email ||
+    !password ||
+    !firstname ||
+    !lastname ||
+    age ||
+    !hearing_level ||
+    !interpreter_group ||
+    !curriculum ||
+    !curr_time ||
+    !institution ||
+    !picture_profile ||
+    !role
+  ) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  //validate email format
+  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!email.match(emailFormat)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+  //validate password format
+  const passwordFormat =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!password.match(passwordFormat)) {
+    return res.status(400).json({ error: "Invalid password format" });
+  }
+
+  next();
+};
+export default validateRegister;
