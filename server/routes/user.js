e127c96843b3436b5730fccdadd46c2bad8e9e4a
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 21:23:04 +0700

    Edit : remove syntax

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,13 @@
 import express from "express";
 // import { User, connectToDatabase } from "./configs/db.config.js";
 //import authenticateUser from "../middlewares/authUser.js";
-import userLogin from "../controllers/userController.js";
+import { userLogin } from "../controllers/userController.js";
+import validateLogin from "../middlewares/validateLogin.js";
 
 const router = express.Router();
-router.post("/login", userLogin);
-//validateLogin, userLogin);
+
+router.post("/", validateLogin, userLogin); // /api/users/login
+
 // router.get("/protected", authenticateUser, (req, res));
 
 export default router;
