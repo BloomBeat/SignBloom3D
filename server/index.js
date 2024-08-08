2031b413990b0f224eb586b676c1378210535886
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 21:25:12 +0700

    Edit : prettier and add userRouter

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import express from "express";
 import { connectToDatabase } from "./configs/db.config.js";
 import router from "./routes/vocab.js"; // Updated import
 import userRouter from "./routes/user.js"; // Updated import
-
 const app = express();
 
 // Connect to the databasew
@@ -12,7 +11,7 @@ connectToDatabase();
 // Middleware, routes, etc.
 app.use(express.json());
 app.use("/api", router);
-app.use("/api/user", userRouter);
+app.use("/api/users", userRouter); // api/users/
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
