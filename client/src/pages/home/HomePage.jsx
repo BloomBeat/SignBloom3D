ac4ec312fa680237d6a9ce9405c8622361a7d3b5
NANAYEMYINTKYI <ayemyint.kyi2004@gmail.com>
2024-08-08 12:56:50 +0700

    fix: mergefile

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { useState, useEffect } from "react";
-import Navbaruser from '../../components/Navbaruser';
-
-// client/src/components/Navbaruser.jsx
-export const Home = () => {
-  const [message, setMessage] = useState("");
-
-  // useEffect(() => {
-  //   fetch("/api")
-  //     .then((response) => response.json())
-  //     .then((data) => setMessage(data.message));
-  // }, []);
-  // return <div className="mt-10 flex w-full flex-col">{message}</div>;
-  return (
-    <div>
-      <Navbaruser></Navbaruser>
-      <div>
-        <FilterStatus></FilterStatus>
-      </div>
-    </div>
- 
-=======
 import React from "react";
 import { useState, useEffect } from "react";
 import Navbaruser from "../../components/Navbaruser";
@@ -36,6 +13,5 @@ export const Home = () => {
   //  return <div className="mt-10 flex w-full flex-col">{message}</div>;
   return (
    <Navbaruser></Navbaruser>
->>>>>>> component-intialization
   )
 };
