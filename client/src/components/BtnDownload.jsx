f07b6fa63ef2eb917c2dbabd931beea20d6b5296
KwinyarutP <kwinyarut2546@gmail.com>
2024-08-08 02:31:29 +0200

    edit: color of button

--- client/src/components/BtnDownload.jsx
+++ client/src/components/BtnDownload.jsx
@@ -1,6 +1,6 @@
 function BtnDownload() {
   return (
-    <button className="rounded bg-primary-base py-2 px-4 text-sm text-white hover:bg-sky-500 active:bg-sky-700">
+    <button className="rounded bg-secondary-base py-2 px-4 text-sm text-white hover:bg-primary-base active:bg-sky-700">
       ดาวน์โหลด
     </button>
   );
