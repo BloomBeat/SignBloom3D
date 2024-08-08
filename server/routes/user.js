f1c2cfdc4e01829face1a4b5d029d33723a3aad0
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 16:37:10 +0700

    Update : Validate login helmet()

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 // import { User, connectToDatabase } from "./configs/db.config.js";
-import authenticateUser from "../middlewares/authUser.js";
+//import authenticateUser from "../middlewares/authUser.js";
 import userLogin from "../controllers/userController.js";
 
 const router = express.Router();
