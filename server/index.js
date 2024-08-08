51ead2fad91fb14b1b20ad2e287a5a8f96d1757d
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 21:25:44 +0700

    Update : change npm->pnpm

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ connectToDatabase();
 
 app.use(express.json());
 app.use("/api", router);
-app.use("/api/users", userRouter); // api/users/
+app.use("/api/login", userRouter); // api/users/
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
