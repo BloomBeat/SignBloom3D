b9853167add5896088cd0fc0ad11e5a56ce666fd
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 15:00:35 +0700

    Update : UserController ,middleware, routes files

---
+++ server/middlewares/validateLogin.js
@@ -0,0 +1,48 @@
+import express from "express";
+import helmet from "helmet"; //add for resolve Content Security Policy (CSP) issue.
+
+const app = express();
+
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+    },
+  })
+);
+
+const validateUser = (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+
+  if (typeof username !== "string" || typeof password !== "string") {
+    return res
+      .status(400)
+      .json({ error: "Username and password must be strings" });
+  }
+
+  if (username.length < 3 || username.length > 20) {
+    return res
+      .status(400)
+      .json({ error: "Username must be between 3 and 20 characters" });
+  }
+
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ error: "Password must be at least 8 characters long" });
+  }
+
+  // If all validations pass, move to the next middleware
+  next();
+};
+
+module.exports = validateUser;
---
+++ server/routes/user.js
@@ -0,0 +1,11 @@
+import express from "express";
+// import { User, connectToDatabase } from "./configs/db.config.js";
+import authenticateUser from "../middlewares/authUser.js";
+import userLogin from "../controllers/userController.js";
+
+const router = express.Router();
+router.post("/login", userLogin);
+//validateLogin, userLogin);
+// router.get("/protected", authenticateUser, (req, res));
+
+export default router;
