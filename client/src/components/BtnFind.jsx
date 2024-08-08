a9b26b8e4b3a2be0bb1b53bc3c71a388c5e9217b
NANAYEMYINTKYI <ayemyint.kyi2004@gmail.com>
2024-08-08 21:02:28 +0700

    fix: Navbar and Botton

--- client/src/components/BtnFind.jsx
+++
@@ -1,9 +0,0 @@
-function BtnFind() {
-  return (
-    <button className="rounded bg-secondary-base py-2 px-4 text-sm text-white hover:bg-primary-base active:bg-sky-700">
-      ค้นหา
-    </button>
-  );
-}
-
-export default BtnFind;
--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -5,12 +5,6 @@ import Navbaruser from "../../components/Navbaruser";
 export const Home = () => {
   const [message, setMessage] = useState("");
 
-  //  useEffect(() => {
-  //    fetch("/api")
-  //      .then((response) => response.json())
-  //      .then((data) => setMessage(data.message));
-  //  }, []);
-  //  return <div className="mt-10 flex w-full flex-col">{message}</div>;
   return (
    <Navbaruser></Navbaruser>
   )
