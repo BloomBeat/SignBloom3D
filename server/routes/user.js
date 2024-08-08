676c6b295ab6c06c23aff58f5fff20c36eed4012
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-08 15:00:35 +0700

    Update : UserController ,middleware, routes files

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,8 +4,8 @@ import authenticateUser from "../middlewares/authUser.js";
 import userLogin from "../controllers/userController.js";
 
 const router = express.Router();
-
-router.post("/login", validateLogin, userLogin);
-router.post("/register", validateRegister, userRegister);
+router.post("/login", userLogin);
+//validateLogin, userLogin);
+// router.get("/protected", authenticateUser, (req, res));
 
 export default router;
