5f03c360aae43dcc0a0459bc866c17d523e3b489
ppearyxx <pearp.pinnuwat@gmail.com>
2024-08-10 02:11:26 +0700

    Update : displayVocab details(only a word)

--- server/routes/vocab.js
+++ server/routes/vocab.js
@@ -2,12 +2,11 @@
 import express from "express";
 import {
   displayVocab,
-  displayVocab,
   vocabSuggestions,
 } from "../controllers/vocabController.js";
 
 const router = express.Router();
 
 router.get("/", vocabSuggestions); // Use the controller function as the route handler
-router.get("/word/:id", displayVocab);
+router.get("/word/:id", displayVocab); // Use the controller function as the route handler
 export default router;
