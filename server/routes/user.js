e8730bfcf03e168a06184b722ec2cc394ce7b5d4
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-08-09 13:43:55 +0700

    add: login and register api

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,7 +5,7 @@ import validateRegister from "../middlewares/validateRegister.js";
 
 const router = express.Router();
 
-router.post("/login", validateLogin, userLogin); // /api/users/login
-router.post("/register", validateRegister, userRegister); // /api/users/register
+router.post("/login", validateLogin, userLogin);
+router.post("/register", validateRegister, userRegister);
 
 export default router;
