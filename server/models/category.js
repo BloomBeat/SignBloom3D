95c69c17694b4912a1b0bb8a273df16dbc4ce2f4
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-09-24 23:18:31 +0700

    Refactor category schema and remove vocabularies field
    The category schema in the server/models/category.js file has been refactored. The vocabularies field has been removed from the schema.
    
    This change was made to simplify the category schema and remove unnecessary complexity.
    
    Co-authored-by: [Author Name]

--- server/models/category.js
+++ server/models/category.js
@@ -1,20 +1,10 @@
 // server/models/category.js
 import mongoose from "mongoose";
 
-const vocabularySchema = new mongoose.Schema({
-  name: { type: String },
-  description: { type: String },
-  parts_of_speech: { type: String },
-  image: { type: String },
-  author: { type: String }, // Add author field
-  created_at: { type: Date },
-  updated_at: { type: Date },
-});
-
 const categorySchema = new mongoose.Schema({
   category: { type: String },
   image: { type: String },
-  vocabularies: { type: [vocabularySchema] },
+  // vocabularies: { type: [vocabularySchema] },
   created_at: { type: Date },
   updated_at: { type: Date },
 });
