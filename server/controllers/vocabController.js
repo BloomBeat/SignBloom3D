623af13377a371070d3b6f117671b8c88eae95a6
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-09-25 00:17:05 +0700

    change: vocabSuggestion, displayVocab rule change after migration

--- server/controllers/vocabController.js
+++ server/controllers/vocabController.js
@@ -1,4 +1,5 @@
 import Category from "../models/category.js";
+import Vocabulary from "../models/vocabulary.js";
 import mongoose from "mongoose";
 
 export const vocabSuggestions = async (req, res) => {
@@ -17,32 +18,78 @@ export const vocabSuggestions = async (req, res) => {
     const regex = find ? new RegExp(`^${find}`, "i") : null;
     const skip = (page - 1) * limit;
 
-    const matchCriteria = {};
-    if (category) matchCriteria.category = category;
-    if (regex) matchCriteria["vocabularies.name"] = { $regex: regex };
-    if (parts_of_speech)
-      matchCriteria["vocabularies.parts_of_speech"] = parts_of_speech;
-    if (author) matchCriteria["vocabularies.author"] = author;
+    // const matchCriteria = {};
+    // if (category) matchCriteria.category = category;
+    // if (regex) matchCriteria["vocabularies.name"] = { $regex: regex };
+    // if (parts_of_speech)
+    //   matchCriteria["vocabularies.parts_of_speech"] = parts_of_speech;
+    // if (author) matchCriteria["vocabularies.author"] = author;
+
+    // const pipeline = [
+    //   { $unwind: "$vocabularies" },
+    //   { $sort: { "vocabularies.updated_at": sortOrder === "asc" ? 1 : -1 } },
+    //   { $match: matchCriteria },
+    //   {
+    //     $facet: {
+    //       suggestions: [
+    //         ...(sortAlphabet ? [{ $sort: { "vocabularies.name": 1 } }] : []),
+    //         { $skip: skip },
+    //         { $limit: Number(limit) },
+    //         {
+    //           $project: {
+    //             // _id: 0, // Uncomment this line if _id needs to be excluded
+    //             name: "$vocabularies.name",
+    //             category: "$category",
+    //             parts_of_speech: "$vocabularies.parts_of_speech",
+    //             description: "$vocabularies.description",
+    //             author: "$vocabularies.author",
+    //             updated_at: "$vocabularies.updated_at",
+    //           },
+    //         },
+    //       ],
+    //       totalResults: [{ $count: "total" }],
+    //     },
+    //   },
+    // ];
+
+    const matchCriteria = {
+      ...(category
+        ? {
+            category_id: new mongoose.Types.ObjectId.createFromHexString(
+              category
+            ),
+          }
+        : {}),
+      ...(regex ? { name: { $regex: regex } } : {}),
+      ...(parts_of_speech ? { parts_of_speech: parts_of_speech } : {}),
+      ...(author ? { author } : {}),
+    };
 
     const pipeline = [
-      { $unwind: "$vocabularies" },
-      { $sort: { "vocabularies.updated_at": sortOrder === "asc" ? 1 : -1 } },
-      { $match: matchCriteria },
+      { $match: matchCriteria }, // Filter the documents
+      { $sort: { updated_at: sortOrder === "asc" ? 1 : -1 } }, // Sort the documents
       {
         $facet: {
           suggestions: [
-            ...(sortAlphabet ? [{ $sort: { "vocabularies.name": 1 } }] : []),
-            { $skip: skip },
-            { $limit: Number(limit) },
+            ...(sortAlphabet ? [{ $sort: { name: 1 } }] : []),
+            { $skip: skip }, // Skip the first n documents
+            { $limit: Number(limit) }, // Limit the number of documents
+            {
+              $lookup: {
+                from: "categories",
+                localField: "category_id",
+                foreignField: "_id",
+                as: "category",
+              },
+            },
             {
               $project: {
-                // _id: 0, // Uncomment this line if _id needs to be excluded
-                name: "$vocabularies.name",
-                category: "$category",
-                parts_of_speech: "$vocabularies.parts_of_speech",
-                description: "$vocabularies.description",
-                author: "$vocabularies.author",
-                updated_at: "$vocabularies.updated_at",
+                name: 1,
+                description: 1,
+                parts_of_speech: 1,
+                author: 1,
+                updated_at: 1,
+                category: { $arrayElemAt: ["$category.category", 0] },
               },
             },
           ],
@@ -51,7 +98,7 @@ export const vocabSuggestions = async (req, res) => {
       },
     ];
 
-    const result = await Category.aggregate(pipeline).exec();
+    const result = await Vocabulary.aggregate(pipeline).exec();
 
     const suggestions = result[0].suggestions;
     const totalCount =
@@ -72,50 +119,52 @@ export const vocabSuggestions = async (req, res) => {
 export const displayVocab = async (req, res) => {
   try {
     const { id } = req.params;
-    const vocabId = mongoose.Types.ObjectId.createFromHexString(id);
+    const vocabId = mongoose.Types.ObjectId(id); // Convert the id to ObjectId
+
     const pipeline = [
-      { $unwind: "$vocabularies" },
-      { $match: { "vocabularies._id": vocabId } },
+      { $match: { _id: vocabId } }, // Find the specific vocabulary by id
       {
         $lookup: {
-          from: "tickets",
-          localField: "vocabularies._id",
+          from: "categories",
+          localField: "category_id",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+      {
+        $lookup: {
+          from: "tickets", // Lookup in the 'tickets' collection
+          localField: "_id", // Match vocabulary id with ticket's vocabulary_id
           foreignField: "vocabulary_id",
           as: "tickets",
         },
       },
       {
-        $facet: {
-          vocabDetails: [
-            {
-              $project: {
-                _id: 0,
-                name: "$vocabularies.name",
-                description: "$vocabularies.description",
-                parts_of_speech: "$vocabularies.parts_of_speech",
-                author: "$vocabularies.author",
-                updated_at: "$vocabularies.updated_at",
-                ticket_status: {
-                  $cond: {
-                    if: { $gt: [{ $size: "$tickets" }, 0] }, // Check if there are tickets
-                    then: { $arrayElemAt: ["$tickets.status", 0] }, // Get the first matching ticket's status
-                    else: null, // If no tickets, return null
-                  },
-                },
-              },
+        $project: {
+          name: 1,
+          description: 1,
+          parts_of_speech: 1,
+          author: 1,
+          updated_at: 1,
+          category: 1,
+          ticket_status: {
+            $cond: {
+              if: { $gt: [{ $size: "$tickets" }, 0] }, // Check if there are tickets
+              then: { $arrayElemAt: ["$tickets.status", 0] }, // Get the first ticket's status
+              else: null, // If no tickets, return null
             },
-          ],
+          },
         },
       },
     ];
 
-    const result = await Category.aggregate(pipeline).exec();
+    const result = await Vocabulary.aggregate(pipeline).exec();
 
-    if (result.length === 0 || result[0].vocabDetails.length === 0) {
+    if (result.length === 0) {
       return res.status(404).json({ error: "Word not found" });
     }
 
-    const vocab = result[0].vocabDetails[0];
+    const vocab = result[0];
     res.status(200).json(vocab);
   } catch (err) {
     console.error("Failed to fetch word:", err);
