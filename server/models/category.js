302586a9c438d952f915c7df70d6efd0d4cb783a
wutpppppppppppppppppp <gansahy@gmail.com>
2024-09-26 11:52:32 +0700

    delete: Schema changed

--- server/models/category.js
+++ server/models/category.js
@@ -1,13 +1,16 @@
 // server/models/category.js
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
-  category: { type: String },
-  image: { type: String },
-  // vocabularies: { type: [vocabularySchema] },
-  created_at: { type: Date },
-  updated_at: { type: Date },
-});
+const categorySchema = new mongoose.Schema(
+  {
+    category: { type: String },
+    image: { type: String },
+    created_at: { type: Date },
+    updated_at: { type: Date },
+  }
+  // Alternatively, you can use the timestamps option to automatically add the created_at and updated_at fields:
+  // { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
 
 const Category = mongoose.model("categories", categorySchema);
 
